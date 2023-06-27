a036e61830c8d0c41ee8b1c679e39e6abe34778f
Sergio <sergiompc12@gmail.com>
2023-06-27 22:56:02 +0100

    fix: clean

--- network_element.py
+++ network_element.py
@@ -1,6 +1,7 @@
 from helpers.network_utils import NetworkElement
 
-
+#works as both edge and relay, the only difference between them
+#is how they behave in the tipology
 if __name__ == '__main__':
     element = NetworkElement()
     element.run_network_element()
